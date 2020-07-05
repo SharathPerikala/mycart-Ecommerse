967d0cee0db767d069a34cb85385e8c7a14df011
Sharath Perikala <sharathperikala@gmail.com>
2020-07-05 22:07:08 +0530

    Updates

---
+++ precache-manifest.370131a3fbec7e57fb3a68dd6f49ea9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf6ae66960a9fca8c7f94934d66ab36",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b412eb5af1e9f52f4b4",
+    "url": "/static/js/2.a3ba2acc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a3ba2acc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea6e1ed40d476a03f95f",
+    "url": "/static/js/main.bbefeec3.chunk.js"
+  },
+  {
+    "revision": "ec2f6939a67a80a5ad85",
+    "url": "/static/js/runtime-main.85ae37d9.js"
+  }
+]);
